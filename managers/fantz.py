e57f6745f08a9d4d44c7bc13dc63ddd7a41f244b
prodigyoff <maryan.kit@gmail.com>
2020-12-07 16:33:37 +0200

    Did some naming fixes

--- managers/fantz.py
+++ managers/fantz.py
@@ -1,29 +1,29 @@
 import re
 
 
-def fill_list_of_degrees(number: int, binary_number_length: int):
+def fill_list_of_powers(decimal_number: int, binary_number_length: int):
     """
-    Returns list of binary degrees from given decimal number
+    Returns list of binary powers from given decimal number
 
-    >>> fill_list_of_degrees(5, 7)
+    >>> fill_list_of_powers(5, 7)
     ['1111101', '11001', '101', '1']
-    >>> fill_list_of_degrees(5, 9)
+    >>> fill_list_of_powers(5, 9)
     ['1001110001', '1111101', '11001', '101', '1']
 
     """
-    degrees = ['1']
+    powers_list = ['1']
     for i in range(binary_number_length):
-        if len(degrees[i]) >= binary_number_length:
+        if len(powers_list[i]) >= binary_number_length:
             break
-        degrees.append(bin(number ** (i + 1))[2:])
-    degrees.reverse()
-    return degrees
+        powers_list.append(bin(decimal_number ** (i + 1))[2:])
+    powers_list.reverse()
+    return powers_list
 
 
-def fantz(binary_number: str, number: int):
+def fantz(binary_number: str, decimal_number: int):
     """
     Running through testcases given in task and a few edge cases.
-    Returns optimal number of replaces needed to represent given binary number as a few degrees of decimal number
+    Returns optimal number of replaces needed to represent given binary number as a few powers of decimal number
 
     >>> fantz('101101101', 5)
     3
@@ -41,20 +41,20 @@ def fantz(binary_number: str, number: int):
     3
     """
     binary_number = binary_number.lstrip('0')
-    counter = 0
-    degrees = fill_list_of_degrees(number, len(binary_number))
+    replaces_counter = 0
+    powers = fill_list_of_powers(decimal_number, len(binary_number))
 
-    for degree in degrees:
-        result_number, replaces_amount = re.subn(degree, '', binary_number)
+    for power in powers:
+        result_number, replaces_amount = re.subn(power, '', binary_number)
         if replaces_amount == 0:
             continue
         binary_number = result_number
-        counter += replaces_amount
+        replaces_counter += replaces_amount
         if binary_number == '':
             break
     if binary_number != '':
         return -1
-    return counter
+    return replaces_counter
 
 
 def main():
