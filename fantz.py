e8d3306f8cf6d766d7d80365859baf0a39c77cbf
prodigyoff <maryan.kit@gmail.com>
2020-12-10 11:18:59 +0200

    Minor changes

--- fantz.py
+++ fantz.py
@@ -8,7 +8,7 @@ def fill_list_of_powers(decimal_number: int, binary_number_length: int) -> list:
     return powers_list[1:]
 
 
-def any_power_in_remainder(powers: list, binary_number: str):
+def any_power_in_remainder(powers: list, binary_number: str) -> bool:
     if not binary_number:
         return True
     for power in powers:
@@ -24,7 +24,7 @@ def fantz(binary_number: str, decimal_number: int) -> int:
         if len(power) > len(binary_number):
             continue
         if power in binary_number:
-            if (secondary_replaces_counter := binary_number.count(power)) != -1 \
+            if (secondary_replaces_counter := binary_number.count(power)) \
                     and any_power_in_remainder(powers, rest := binary_number.replace(power, '')) \
                     and (len(rest) < 2 or rest[-1] == binary_number[-1]):
                 binary_number = rest
